f2a4616ef4c810c8c75ccdfd811d62be57a28e9d
davidadeola_ <98563443+davidadeola@users.noreply.github.com>
2022-04-08 01:07:01 +0100

    First commit

---
+++ react-gun/src/App.js
@@ -0,0 +1,81 @@
+import { useEffect, useState, useReducer } from 'react';
+import Gun from 'gun';
+
+const gun = Gun({
+  peers: [
+    'http://localhost:3030/gun'
+  ]
+})
+
+const initialState = {
+  messages: []
+}
+
+function reducer(state, message) {
+  return {
+    messages: [message, ...state.messages]
+  }
+}
+
+export default function App() {
+  const [formState, setForm] = useState({
+    name: '', message: ''
+  })
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    const messages = gun.get('messages')
+    messages.map().on(m => {
+      dispatch({
+        name: m.name,
+        message: m.message,
+        createdAt: m.createdAt
+      })
+    })
+  }, [])
+
+  function onChange(event) {
+    setForm({ ...formState, [event.target.name]: event.target.value })
+  }
+
+  function saveMessage() {
+    const messages = gun.get('messages')
+    messages.set({
+      name: formState.name,
+      message: formState.message,
+      createdAt: Date.now()
+    })
+    setForm({
+      name: '', message: ''
+    })
+  }
+
+  return (
+    <div style={{ padding: 30 }}>
+      <input
+        onChange={onChange}
+        placeholder="Name"
+        name="name"
+        value={formState.name}
+      />
+      <input
+        onChange={onChange}
+        placeholder="Message"
+        name="message"
+        value={formState.message}
+      />
+      <button onClick={saveMessage}>Send Message</button>
+      {
+        state.messages.map((message) => (
+        <div key={message.createdAt}>
+          <h2>{message.message}</h2>
+          <h3>From: {message.name}</h3>
+          <p>Date: {message.createdAt}</p>
+        </div>
+      ))}
+    </div>
+  );
+
+}
+
